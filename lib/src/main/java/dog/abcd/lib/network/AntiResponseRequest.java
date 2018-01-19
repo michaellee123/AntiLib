35a590818535286c2d4d4c56ee51bf45601a784a
Michael Lee <jjjamdmin@outlook.com>
2018-01-19 10:42:03 +0800

    使用1.1版本的volley

--- lib/src/main/java/dog/abcd/lib/network/AntiResponseRequest.java
+++ lib/src/main/java/dog/abcd/lib/network/AntiResponseRequest.java
@@ -13,28 +13,28 @@ import com.android.volley.toolbox.HttpHeaderParser;
  *         <b> create at </b>2017/2/21 下午 12:57
  */
 public class AntiResponseRequest extends Request<NetworkResponse> {
-    private Response.Listener<NetworkResponse> mListener;
+	private Response.Listener<NetworkResponse> mListener;
 
-    public AntiResponseRequest(int method, String url, Response.Listener<NetworkResponse> listener, Response.ErrorListener errorListener) {
-        super(method, url, errorListener);
-        this.mListener = listener;
-    }
+	public AntiResponseRequest(int method, String url, Response.Listener<NetworkResponse> listener, Response.ErrorListener errorListener) {
+		super( method, url, errorListener );
+		this.mListener = listener;
+	}
 
-    @Override
-    protected void onFinish() {
-        super.onFinish();
-        mListener = null;
-    }
+	@Override
+	public void cancel() {
+		super.cancel();
+		this.mListener = null;
+	}
 
-    @Override
-    protected Response<NetworkResponse> parseNetworkResponse(NetworkResponse response) {
-        return Response.success(response, HttpHeaderParser.parseCacheHeaders(response));
-    }
+	@Override
+	protected Response<NetworkResponse> parseNetworkResponse(NetworkResponse response) {
+		return Response.success( response, HttpHeaderParser.parseCacheHeaders( response ) );
+	}
 
-    @Override
-    protected void deliverResponse(NetworkResponse response) {
-        if (mListener != null) {
-            mListener.onResponse(response);
-        }
-    }
+	@Override
+	protected void deliverResponse(NetworkResponse response) {
+		if (mListener != null) {
+			mListener.onResponse( response );
+		}
+	}
 }
